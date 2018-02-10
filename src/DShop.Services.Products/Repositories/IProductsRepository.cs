78a25d8a0933252dd37aeeb2d96d8b0fcb85bc23
Dariusz Pawlukiewicz <foreverframenet@gmail.com>
2018-02-10 17:01:12 +0100

    Added product entitiy

---
+++ src/DShop.Services.Products/Repositories/IProductsRepository.cs
@@ -0,0 +1,10 @@
+using DShop.Common.Databases;
+using DShop.Services.Products.Entities;
+
+namespace DShop.Services.Products.Repositories
+{
+    public interface IProductsRepository : IRepository<Product>
+    {
+         
+    }
+}
