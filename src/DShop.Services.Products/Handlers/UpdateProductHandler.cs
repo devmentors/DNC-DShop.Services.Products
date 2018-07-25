6adbf58a7a9f95397aad1d49e641443f49b92a05
GooRiOn <foreverframenet@gmail.com>
2018-07-25 19:01:53 +0200

    Changed command handlers so it uses repo instead of app service. Introducted handler for command execution.

--- src/DShop.Services.Products/Handlers/UpdateProductHandler.cs
+++ src/DShop.Services.Products/Handlers/UpdateProductHandler.cs
@@ -1,28 +1,51 @@
 ﻿using DShop.Common.Handlers;
 using DShop.Common.RabbitMq;
+using DShop.Common.Types;
 using DShop.Messages.Commands.Products;
 using DShop.Messages.Events.Products;
-using DShop.Services.Products.Services;
+using DShop.Services.Products.Repositories;
 using System.Threading.Tasks;
 
 namespace DShop.Services.Products.Handlers
 {
     public sealed class UpdateProductHandler : ICommandHandler<UpdateProduct>
     {
-        private readonly IProductsService _productsService;
+        private readonly IProductsRepository _productsRepository;
+        private readonly IHandler _handler;
         private readonly IBusPublisher _busPublisher;
 
 
-        public UpdateProductHandler(IProductsService productsService, IBusPublisher busPublisher)
+        public UpdateProductHandler(
+            IProductsRepository productsRepository,
+            IHandler handler,
+            IBusPublisher busPublisher)
         {
-            _productsService = productsService;
+            _productsRepository = productsRepository;
+            _handler = handler;
             _busPublisher = busPublisher;
         }
 
         public async Task HandleAsync(UpdateProduct command, ICorrelationContext context)
-        {
-            await _productsService.UpdateAsync(command.Id, command.Name, command.Description, command.Price);
-            await _busPublisher.PublishEventAsync(new ProductUpdated(command.Id), context);
-        }
+            => await _handler
+                .Handle(async () =>
+                {
+                    var product = await _productsRepository.GetAsync(command.Id);
+
+                    if (product == null)
+                    {
+                        throw new DShopException("Product does not exist.");
+                    }
+
+                    product.SetName(command.Name);
+                    product.SetDescription(command.Description);
+                    product.SetPrice(command.Price);
+
+                    await _productsRepository.UpdateAsync(product);
+                })
+                .OnSuccess(async () =>
+                {
+                    await _busPublisher.PublishEventAsync(new ProductUpdated(command.Id), context);
+                })
+                .ExecuteAsync();
     }
 }
