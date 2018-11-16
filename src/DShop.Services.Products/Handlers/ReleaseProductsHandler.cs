d0d09eedf323af4dd4c9934abdf7a2ef2b3bbc22
Piotr Gankiewicz <spetz90@gmail.com>
2018-11-16 19:36:39 +0100

    Logging info

--- src/DShop.Services.Products/Handlers/ReleaseProductsHandler.cs
+++ src/DShop.Services.Products/Handlers/ReleaseProductsHandler.cs
@@ -5,6 +5,7 @@ using DShop.Common.RabbitMq;
 using DShop.Services.Products.Messages.Commands;
 using DShop.Services.Products.Messages.Events;
 using DShop.Services.Products.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace DShop.Services.Products.Handlers
 {
@@ -12,26 +13,33 @@ namespace DShop.Services.Products.Handlers
     {
         private readonly IBusPublisher _busPublisher;
         private readonly IProductsRepository _productsRepository;
+        private readonly ILogger<ReleaseProductsHandler> _logger;
 
         public ReleaseProductsHandler(IBusPublisher busPublisher,
-            IProductsRepository productsRepository)
+            IProductsRepository productsRepository,
+            ILogger<ReleaseProductsHandler> logger)
         {
             _busPublisher = busPublisher;
             _productsRepository = productsRepository;
+            _logger = logger;
         }
 
         public async Task HandleAsync(ReleaseProducts command, ICorrelationContext context)
         {
             foreach ((Guid productId, int quantity) in command.Products)
             {
+                _logger.LogInformation($"Releasing a product: '{productId}' ({quantity})");
                 var product = await _productsRepository.GetAsync(productId);
                 if (product == null)
                 {
+                    _logger.LogInformation($"Product was not found: '{productId}' (can't release).");
+
                     continue;
                 }
 
                 product.SetQuantity(product.Quantity + quantity);
                 await _productsRepository.UpdateAsync(product);
+                _logger.LogInformation($"Released a product: '{productId}' ({quantity})");
             }
 
             await _busPublisher.PublishAsync(new ProductsReleased(command.OrderId,
--- src/DShop.Services.Products/Handlers/ReserveProductsHandler.cs
+++ src/DShop.Services.Products/Handlers/ReserveProductsHandler.cs
@@ -5,6 +5,7 @@ using DShop.Common.RabbitMq;
 using DShop.Services.Products.Messages.Commands;
 using DShop.Services.Products.Messages.Events;
 using DShop.Services.Products.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace DShop.Services.Products.Handlers
 {
@@ -12,26 +13,33 @@ namespace DShop.Services.Products.Handlers
     {
         private readonly IBusPublisher _busPublisher;
         private readonly IProductsRepository _productsRepository;
+        private readonly ILogger<ReserveProductsHandler> _logger;
 
         public ReserveProductsHandler(IBusPublisher busPublisher,
-            IProductsRepository productsRepository)
+            IProductsRepository productsRepository,
+            ILogger<ReserveProductsHandler> logger)
         {
             _busPublisher = busPublisher;
             _productsRepository = productsRepository;
+            _logger = logger;
         }
 
         public async Task HandleAsync(ReserveProducts command, ICorrelationContext context)
         {
             foreach ((Guid productId, int quantity) in command.Products)
             {
+                _logger.LogInformation($"Reserving a product: '{productId}' ({quantity})");
                 var product = await _productsRepository.GetAsync(productId);
                 if (product == null)
                 {
+                    _logger.LogInformation($"Product was not found: '{productId}' (can't reserve).");
+
                     continue;
                 }
 
                 product.SetQuantity(product.Quantity - quantity);
                 await _productsRepository.UpdateAsync(product);
+                _logger.LogInformation($"Reserved a product: '{productId}' ({quantity})");
             }
 
             await _busPublisher.PublishAsync(new ProductsReserved(command.OrderId,
